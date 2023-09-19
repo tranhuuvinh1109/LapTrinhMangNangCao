1459196dc346e7f1406040704f0fa6394733ff79
taed13 <taedtech13@gmail.com>
2023-09-19 22:42:15 +0700

    config

--- src/pages/DashboardAdmin/index.jsx
+++ src/pages/DashboardAdmin/index.jsx
@@ -1,15 +1,9 @@
-import React from 'react'
-import './DashboardAdmin.css'
-
+import React from "react";
+import "./DashboardAdmin.css";
 
 const DashboardAdmin = () => {
-	return (
-		<div>
-			<h1>
-				DashboardAdmin
-			</h1>
-		</div>
-	)
-}
+  return;
+  <div>aa</div>;
+};
 
-export default DashboardAdmin
+export default DashboardAdmin;
--- src/pages/LayoutAdmin/index.jsx
+++ src/pages/LayoutAdmin/index.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import './LayoutAdmin.css'
-import { Link, Outlet } from 'react-router-dom';
-
+import React from "react";
+import "./LayoutAdmin.css";
+import { Link, Outlet } from "react-router-dom";
+import { Sidebar } from "../../components";
+import  from "../../components/NavbarAdmin";
+import HeaderStats frmm "../../"
 const LayoutAdmin = () => {
-	return (
-		<div>
-			<Link to='/'>Return home</Link>
-			<Outlet />
-		</div>
-	)
-}
+  return (
+    <div>
+      <Sidebar />
+      {/* <Link to='/'>Return home</Link> */}
+      <div>
+        <NavbarAdmin />
+<HeaderStats/>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
 
-export default LayoutAdmin
+export default LayoutAdmin;
