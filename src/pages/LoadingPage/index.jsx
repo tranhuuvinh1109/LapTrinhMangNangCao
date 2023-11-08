6c0988d1e001064a35a71c139054d6cc6c7381db
Tấn Cuỳnh <binphan120302@gmail.com>
2023-11-08 10:57:23 +0700

    fix loading

--- src/pages/LoadingPage/index.jsx
+++ src/pages/LoadingPage/index.jsx
@@ -4,7 +4,7 @@ import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 const LoadingPage = () => {
   return (
     <div
-      className="w-full h-screen  flex justify-center items-center z-50 absolute top-0 bottom-0 right-0 left-0"
+      className="fixed flex justify-center items-center z-[10000] top-0 bottom-0 right-0 left-0 overflow-hidden"
       style={{ background: 'rgba(0,0,0,0.5)' }}
     >
       <AiOutlineLoading3Quarters fontSize={60} className="animate-spin text-white" />
